e0297f40a6fe248880601d63f13465d7b8fdbb32
Yaroslav Sivakov <yar3333@gmail.com>
2013-09-27 14:03:44 +0400

    * great yuidoc2haxe improvements

---
+++ tools/yuidoc2haxe/src/Tools.hx
@@ -0,0 +1,37 @@
+package ;
+
+class KlassTools
+{
+	public static function getExtends(klass:Klass) : String
+	{
+		return Reflect.field(klass, "extends");
+	}
+	
+	public static function isStatic(klass:Klass) : Bool
+	{
+		return Reflect.field(klass, "static") == 1;
+	}
+}
+
+class ItemTools
+{
+	public static function getDefault(item:Item) : String
+	{
+		return Reflect.field(item, "default");
+	}
+	
+	public static function getClass(item:Item) : String
+	{
+		return Reflect.field(item, "class");
+	}
+	
+	public static function getReturn(item:Item) : { description:String, type:String }
+	{
+		return Reflect.field(item, "return");
+	}
+	
+	public static function setReturn(item:Item, ret:{ description:String, type:String }) : Void
+	{
+		return Reflect.setField(item, "return", ret);
+	}
+}
---
+++ tools/yuidoc2haxe/src/YuiDoc.hx
@@ -0,0 +1,9 @@
+package ;
+
+typedef YuiDoc =
+{
+	  project : Dynamic
+	, files: Dynamic
+	, classes: Dynamic<Klass>
+	, classitems: Array<Item>
+}
