2c35e2d1d5dc652d8284ab468d0a6421f8b7f367
Yaroslav <yar3333@gmail.com>
2013-09-27 21:56:56 +0400

    + optional params

--- tools/yuidoc2haxe/src/Param.hx
+++ tools/yuidoc2haxe/src/Param.hx
@@ -5,4 +5,6 @@ typedef Param =
 	  name : String //"bubbles",
 	, description : String // "Indicates whether the event will bubble through the display list.",
 	, type : String // "Boolean"
+	, optional : Bool
+	, optdefault : String
 }
