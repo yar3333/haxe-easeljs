7d23e5125c14caa1b692b1997fce7d0187d10736
Yaroslav Sivakov <yar3333@gmail.com>
2014-12-01 17:58:59 +0300

    * test

--- test/Main.hx
+++ test/Main.hx
@@ -1,9 +1,25 @@
-import easeljs.Bitmap;
-import easeljs.ColorFilter;
+import easeljs.Shape;
 import easeljs.Stage;
 import js.Browser;
 import js.html.CanvasElement;
-import js.html.Image;
+import js.html.CanvasRenderingContext2D;
+
+class MyShape extends Shape
+{
+	override public function draw(ctx:js.html.CanvasRenderingContext2D, ?ignoreCache:Bool):Bool 
+	{
+		trace("draw cacheCanvas = " + (cacheCanvas != null) + "; ignoreCache = " + ignoreCache);
+		
+		if (cacheCanvas == null)
+		{
+			trace("cache");
+			cache(0, 0, 100, 100);
+		}
+		
+		trace("inner draw cacheCanvas = " + (cacheCanvas != null) + "; ignoreCache = " + ignoreCache);
+		return super.draw(ctx, ignoreCache);
+	}
+}
 
 class Main
 {
@@ -13,19 +29,16 @@ class Main
 		
 		var stage = new Stage(canvas);
 		
-		var img = new Image();
-		img.onload = function(_)
-		{
-			var bmp = new Bitmap(img);
-			
-			bmp.filters = [ new ColorFilter(1, 1, 1, 1, 255, 0, 0) ];
-			bmp.cache(0, 0, 100, 100);
-			
-			stage.addChild(bmp);
-			
-			stage.update();
-			
-		};
-		img.src = 'фон1 copy 2.png';
+		var shape = new MyShape();
+		stage.addChild(shape);
+		
+		shape.graphics
+			.beginStroke("red")
+			.moveTo(0, 0)
+			.lineTo(100, 100)
+			.endStroke();
+		
+		trace("update");
+		stage.update();
 	}
 }
